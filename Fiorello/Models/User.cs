e7b5edbc51ff9cf37cad2ea0f73576ae9d907857
Tamerlan9129 <tamerlankha@code.edu.az>
2022-11-11 17:52:26 +0400

    new

---
+++ Fiorello/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Fiorello.Models
+{
+    public class User:IdentityUser
+    {
+        public string Fullname { get; set; }
+    }
+}
--- Fiorello/Program.cs
+++ Fiorello/Program.cs
@@ -1,5 +1,7 @@
 using Fiorello.DAL;
 using Fiorello.Helpers;
+using Fiorello.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +9,19 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IFileService, FileService>();
 var connectionString = builder.Configuration.GetConnectionString("Default");
 builder.Services.AddDbContext<AppDbContext>(x=>x.UseSqlServer(connectionString));
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
 
+    options.User.RequireUniqueEmail = true;
+    options.Lockout.MaxFailedAccessAttempts = 3;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromHours(3);
+})
+     .AddEntityFrameworkStores<AppDbContext>();
 var app = builder.Build();
 
 app.MapControllerRoute(
@@ -18,5 +32,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=home}/{action=Index}/{id?}"
     );
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseStaticFiles();   
 app.Run();
